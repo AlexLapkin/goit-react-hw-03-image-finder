1d3805e5f97194ad0658989c57dfcb416eb43f26
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2023-10-29 23:25:37 +0200

    update

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -33,8 +33,8 @@ state = {
             //value={searchWord}
             className={css.searchbar_input}
             type="text"
-            //autocomplete="off"
-            //autofocus
+            autocomplete="off"
+            autofocus
             placeholder="Search images and photos"
             onChange={this.handleInputChange}
           />
